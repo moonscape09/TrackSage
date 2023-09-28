0e81be3111c0351c31ddb4115ea640c5d01eda23
moonscape09 <munim.adil@outlook.com>
2023-09-27 23:09:08 -0400

    Set up frontend and render first component

---
+++ client/src/components/DayCards.js
@@ -0,0 +1,16 @@
+import React from "react";
+ 
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+ 
+// We import NavLink to utilize the react router.
+// import { NavLink } from "react-router-dom";
+ 
+// Here, we display our Navbar
+export default function DayCards() {
+ return (
+   <div>
+        <p>Hello world</p>
+    </div>
+ );
+}
